813849b20be10f787ce044674d56e92cab381aa4
Lashchik <Pondohva69@mail.ru>
2023-02-27 21:20:25 +0300

    решил задачу с собакой

--- Example008_Sobaka/Program.cs
+++ Example008_Sobaka/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int count = 0;
+int distance = 10000;
+int FirstFriendSpeed = 1;
+int SecondFriendSpeed = 2;
+int DogSpeed = 5;
+int friend = 2;
+int time = 0;
+
+while (distance > 10)
+{
+    
+    if (friend == 1)
+    {
+    time = (distance / (FirstFriendSpeed + DogSpeed));
+    friend = 2;
+    }
+    if (friend == 2)
+    {
+        time = (distance / (SecondFriendSpeed + DogSpeed));
+     friend = 1;
+     }
+    distance = (distance - (FirstFriendSpeed + SecondFriendSpeed) * time);
+    count = count + 1;
+}
+    Console.WriteLine("Собака пробежала " + count);
